c1e5898aea71065210a0b8d6796b2985a1f699cf
Anjali MahawR <anjalimahawar454@gmail.com>
2022-06-15 01:42:18 -0400

    js variabls

---
+++ coustom.js
@@ -0,0 +1,5 @@
+var a='MY';
+const B='NAME';
+let c='IS';
+
+console.log(a+' '+B+" "+`${c}`+' Anjali')
---
+++ js/script.js
@@ -0,0 +1,13 @@
+const A=10;
+const B=14;
+const C=13;
+const D=19;
+var result=A+B+C+D;
+console.log('Addition is '+result)
+
+
+var a1='var';
+const E='let';
+let c1='const';
+
+console.log('Always'+' '+"declare" +` JavaScript `+'variables with '+a1+' '+E+` and ${c1}`)
